94a67b9b9eb5b91fb18db6ed87a4bf0af7858cf0
Saikumar Raghunatha <Saikumar_Raghunatha@epam.com>
2020-02-19 16:37:41 +0530

    now adds numbers

--- add.java
+++ add.java
@@ -2,8 +2,10 @@ import java.util.*;
 import java .io.*;
 public class Arithmetic{
 	public static void main(String ar[]){
-		public void Addition(){
-			System.out.println("inside addition");
+		public void Addition(int a,int b){
+			a=90;
+			b=8;
+			System.out.println(a+b);
 		}
 	}
 }
