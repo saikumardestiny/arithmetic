e3c57158554ee08e7f6bac910dbdfa166d2284a5
saikumar destiny <Saikumar_Raghunatha@epam.com>
2020-02-19 16:17:07 +0530

    prints addition function

---
+++ add.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java .io.*;
+public class Arithmetic{
+	public static void main(String ar[]){
+		public void Addition(){
+			System.out.println("inside addition");
+		}
+	}
+}
