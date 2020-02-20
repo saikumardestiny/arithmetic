712e3f09b04e8d6183e0752840ac9e6da49d1088
Saikumar Raghunatha <Saikumar_Raghunatha@epam.com>
2020-02-20 17:01:03 +0530

    only develop1

--- add.java
+++ add.java
@@ -4,12 +4,9 @@ public class Arithmetic{
 	public static void main(String ar[]){
 		public void Addition(int a,int b){
 			System.out.println("this line is written in develop branch");
-<<<<<<< HEAD
 			System.out.println("from sai nike");
 			System.out.println("without access to sai nike");
-=======
 			System.out.println("this line is also in develop1 branch")
->>>>>>> develop
 			a=90;
 			b=8;
 			System.out.println(a+b);
