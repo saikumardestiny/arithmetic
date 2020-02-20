8a8884d8e1c70945523d25fd21f1bf235016ff00
Saikumar Raghunatha <Saikumar_Raghunatha@epam.com>
2020-02-20 16:51:01 +0530

    second develop

--- add.java
+++ add.java
@@ -4,7 +4,7 @@ public class Arithmetic{
 	public static void main(String ar[]){
 		public void Addition(int a,int b){
 			System.out.println("this line is written in develop branch");
-			System.out.println("this line is also in develop1 branch")
+			System.out.println("this line is also in develop2 branch")
 			a=90;
 			b=8;
 			System.out.println(a+b);
